711e80ebd7f179626c1ec6e464a3d8e47a9797d0
PrathumP <prathumpandey2002@gmail.com>
2024-04-13 16:47:19 +0530

    Model uploaded

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+from leafdetection import predict
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title("Dehao's Simple Image Classification App")
+st.write("")
+
+file_up = st.file_uploader("Upload an image", type="jpg")
+
+if file_up is not None:
+    image = Image.open(file_up)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Just a second...")
+    labels = predict(file_up)
+    st.write(labels)
